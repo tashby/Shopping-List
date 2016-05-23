7823f6df0f1d6e7289aa25802aa70c8aca482f34
Tashby <tashby2714@gmail.com>
2016-05-23 08:08:39 -0400

    fixed

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,14 @@ $(document).ready(function(){
 		// TODO: Add a li with an id to target when we need to delete a li from the shopping cart
 		// e.g <li id='+text+'>text</li>
 		
-		$('ul').prepend("<li>"+text+"</li>");
+		$('ul').prepend("<li id=cart>"+ text +"</li>");
 
 	})
 
 	// TODO: Have a function that deletes the clicked item in the cart,
 	// Hint: get the id of the item clicked and remove it that way
-	$('ul').click(function(){
-		$('ul').children().remove();
+	$('ul').click(function(e){
+	 $(e.target).remove();
+
 	})
 });
