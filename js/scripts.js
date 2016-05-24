8e354871bdb22261afa1474bc518276a3a52fb1e
Tashby <tashby2714@gmail.com>
2016-05-24 09:11:44 -0400

    updated javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,12 @@ $(document).ready(function(){
 	$('li').click(function(){
 		var text=$(this).text();
 
-		// TODO: Add a li with an id to target when we need to delete a li from the shopping cart
-		// e.g <li id='+text+'>text</li>
 		
 		$('ul').prepend("<li id=cart>"+ text +"</li>");
 
 	})
 
-	// TODO: Have a function that deletes the clicked item in the cart,
-	// Hint: get the id of the item clicked and remove it that way
+	
 	$('ul').click(function(e){
 	 $(e.target).remove();
 
